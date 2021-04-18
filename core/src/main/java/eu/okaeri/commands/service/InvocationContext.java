de7af0d2180ebc03c1ff64ce3a42540466e3cb06
PinkLolicorn <pinklolicorngithub@daffit.pl>
2021-04-18 12:48:25 +0200

    Add CommandsUnknownErrorEvent, release 0.4.5

--- core/src/main/java/eu/okaeri/commands/service/InvocationContext.java
+++ core/src/main/java/eu/okaeri/commands/service/InvocationContext.java
@@ -2,23 +2,33 @@ package eu.okaeri.commands.service;
 
 import eu.okaeri.commands.meta.CommandMeta;
 import eu.okaeri.commands.meta.ExecutorMeta;
+import eu.okaeri.commands.meta.ServiceMeta;
 import lombok.Data;
 
 @Data
 public class InvocationContext {
 
-    public static InvocationContext of(CommandMeta command, ExecutorMeta executor, String label, String args) {
+    public static InvocationContext of(CommandMeta command, String label, String args) {
         InvocationContext context = new InvocationContext();
         context.command = command;
-        context.executor = executor;
         context.label = label;
         context.args = args;
         return context;
     }
 
     private CommandMeta command;
-    private ExecutorMeta executor;
-
     private String label;
     private String args;
+
+    public ServiceMeta getService() {
+        return this.command.getService();
+    }
+
+    public ExecutorMeta getExecutor() {
+        return this.command.getExecutor();
+    }
+
+    public boolean isAsync() {
+        return this.getExecutor().isAsync();
+    }
 }
