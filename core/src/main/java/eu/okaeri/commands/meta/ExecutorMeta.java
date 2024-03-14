eac13771d3a9e0576fed5f29b479a77c4f7aabe5
Sandra <sandra@okaeri.cloud>
2024-03-14 14:59:16 +0100

    Fix pattern not being passed through Commands#resolveText

--- core/src/main/java/eu/okaeri/commands/meta/ExecutorMeta.java
+++ core/src/main/java/eu/okaeri/commands/meta/ExecutorMeta.java
@@ -43,56 +43,59 @@ public class ExecutorMeta {
             ? Collections.singletonList(method.getName().startsWith("_") ? "" : method.getName())
             : Arrays.asList(executor.pattern());
 
-        return patterns.stream().map(pattern -> {
+        return patterns.stream()
+            .map(commands::resolveText)
+            .map(pattern -> {
 
-            // create
-            ExecutorMeta cmdExecutor = new ExecutorMeta();
-            cmdExecutor.method = method;
+                // create
+                ExecutorMeta cmdExecutor = new ExecutorMeta();
+                cmdExecutor.method = method;
 
-            List<ArgumentMeta> arguments = new ArrayList<>();
-            for (int i = 0; i < method.getParameters().length; i++) {
-                Parameter parameter = method.getParameters()[i];
-                if (!ArgumentMeta.isArg(parameter)) {
-                    continue;
+                List<ArgumentMeta> arguments = new ArrayList<>();
+                for (int i = 0; i < method.getParameters().length; i++) {
+                    Parameter parameter = method.getParameters()[i];
+                    if (!ArgumentMeta.isArg(parameter)) {
+                        continue;
+                    }
+                    arguments.add(ArgumentMeta.of(commands, parameter, i));
                 }
-                arguments.add(ArgumentMeta.of(commands, parameter, i));
-            }
-            cmdExecutor.arguments = Collections.unmodifiableList(arguments);
+                cmdExecutor.arguments = Collections.unmodifiableList(arguments);
 
-            cmdExecutor.pattern = PatternMeta.of(commands, patternPrefix, pattern, cmdExecutor.arguments, emptyPattern);
-            cmdExecutor.completion = CompletionMeta.of(commands, method);
-            cmdExecutor.description = commands.resolveText(executor.description());
-            cmdExecutor.usage = commands.resolveText(executor.usage());
-            cmdExecutor.index = indexCounter.getAndIncrement();
+                cmdExecutor.pattern = PatternMeta.of(commands, patternPrefix, pattern, cmdExecutor.arguments, emptyPattern);
+                cmdExecutor.completion = CompletionMeta.of(commands, method);
+                cmdExecutor.description = commands.resolveText(executor.description());
+                cmdExecutor.usage = commands.resolveText(executor.usage());
+                cmdExecutor.index = indexCounter.getAndIncrement();
 
-            // validate if all arguments used in method definition ale present in the pattern
-            for (ArgumentMeta argument : cmdExecutor.arguments) {
+                // validate if all arguments used in method definition ale present in the pattern
+                for (ArgumentMeta argument : cmdExecutor.arguments) {
 
-                String argumentName = argument.getName();
-                String patternRaw = cmdExecutor.pattern.getRaw();
+                    String argumentName = argument.getName();
+                    String patternRaw = cmdExecutor.pattern.getRaw();
 
-                if (!cmdExecutor.pattern.getElementByName(argumentName).isPresent()) {
-                    throw new IllegalArgumentException("method argument '" + argumentName + "' not found in the pattern '" + patternRaw + "' from [method: " + method + "]");
-                }
-            }
-
-            // validate if all pattern arguments are used in the method
-            for (PatternElement element : cmdExecutor.pattern.getElements()) {
-
-                if (element instanceof StaticElement) {
-                    continue;
+                    if (!cmdExecutor.pattern.getElementByName(argumentName).isPresent()) {
+                        throw new IllegalArgumentException("method argument '" + argumentName + "' not found in the pattern '" + patternRaw + "' from [method: " + method + "]");
+                    }
                 }
 
-                String elementName = element.getName();
-                String patternRaw = cmdExecutor.pattern.getRaw();
-                boolean present = cmdExecutor.arguments.stream().anyMatch(argumentMeta -> elementName.equals(argumentMeta.getName()));
+                // validate if all pattern arguments are used in the method
+                for (PatternElement element : cmdExecutor.pattern.getElements()) {
 
-                if (!present) {
-                    throw new IllegalArgumentException("argument '" + elementName + "' from pattern '" + patternRaw + "' not found in [method: " + method + "]");
+                    if (element instanceof StaticElement) {
+                        continue;
+                    }
+
+                    String elementName = element.getName();
+                    String patternRaw = cmdExecutor.pattern.getRaw();
+                    boolean present = cmdExecutor.arguments.stream().anyMatch(argumentMeta -> elementName.equals(argumentMeta.getName()));
+
+                    if (!present) {
+                        throw new IllegalArgumentException("argument '" + elementName + "' from pattern '" + patternRaw + "' not found in [method: " + method + "]");
+                    }
                 }
-            }
 
-            return cmdExecutor;
-        }).collect(Collectors.toList());
+                return cmdExecutor;
+            })
+            .collect(Collectors.toList());
     }
 }
